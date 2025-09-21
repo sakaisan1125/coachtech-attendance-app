468081a9684c94d49867550a9a953aff6ebdbd46
坂井　理 <sakaisan0908@gmail.com>
2025-09-21 20:33:07 +0900

    一般ユーザーのログイン認証テストファイル作成

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -17,7 +17,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => bcrypt('password123'), // テスト用の固定パスワード
+            'role' => 'user', // 必要なら追加
         ];
     }
 }
